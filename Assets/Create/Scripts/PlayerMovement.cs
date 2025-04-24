e78f16eb699b1eadb4d083abbedd638f5adbdd96
KhiaoKutx <tatsapong.jira@bumail.net>
2025-04-24 23:15:38 +0800

    player walk&jump

--- Assets/Create/Scripts/PlayerMovement.cs
+++ Assets/Create/Scripts/PlayerMovement.cs
@@ -1,16 +1,51 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb2d;
+
+    float move;
+    [SerializeField] float speed;
+
+    [SerializeField] float jumpForce;
+    [SerializeField] bool isJumping;
+    
+    
     void Start()
     {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        //walk
+        move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
+        
+        //jump
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+            Debug.Log("jump!");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionExit2D(Collision2D other)
     {
-        
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
     }
 }
